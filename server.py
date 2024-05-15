aa62d107a510f75c99033aa64bef79ac0b057784
Allen Zheng <zhenga8533@gmail.com>
2024-05-15 16:24:21 -0400

    added server shutdown functionality

--- server.py
+++ server.py
@@ -12,19 +12,20 @@ BUFFER = 64
 # Data functions
 def load_data(file) -> dict:
     try:
-        with open(file, 'r') as f:
+        with open('./db/' + file, 'r') as f:
             return json.load(f)
     except FileNotFoundError:
         return {}
 
 def save_data(file: str, data: dict):
-    with open(file, 'w') as f:
+    with open('./db/' + file, 'w') as f:
         json.dump(data, f, indent=4)
 
 # Data variables
 core = load_data('data.json')
 users = load_data('users.json')
 lock = threading.Lock()
+running = True
 
 # Command handlers
 def handle_command(data: dict, conn: socket.socket, addr: tuple) -> bool:
@@ -42,12 +43,11 @@ def handle_command(data: dict, conn: socket.socket, addr: tuple) -> bool:
         if username:
             with lock:
                 if version not in users:
-                    users[version] = {"total": 0, "username": {}}
+                    users[version] = {"total": 0, "usernames": {}}
                 if username not in users[version]:
                     users[version]["total"] += 1
 
-                users[version]["username"][username] = datetime.datetime.now().isoformat()
-                save_data('users.json', users)
+                users[version]["usernames"][username] = datetime.datetime.now().isoformat()
     elif command == 'ch':  # Crystal Hollows
         pass
     elif command == 'dm':  # Dwarven Mines
@@ -74,14 +74,35 @@ def handle_client(conn, addr):
 
     conn.close()
 
+def handle_shutdown():
+    global running
+    while running:
+        command = input()
+        if command == 'shutdown':
+            print('[SHUTDOWN] Initiating server shutdown...')
+            running = False
+
+    print('[SHUTDOWN] Saving data...')
+    save_data('users.json', users)
+
 # Start server
 def start():
     server.listen()
+    server.settimeout(10)
+    threading.Thread(target=handle_shutdown).start()
     print(f'[LISTENING] Server is listening on {SERVER}')
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
+
+    global running
+    while running:
+        try:
+            conn, addr = server.accept()
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
+        except socket.timeout:
+            continue
+
+    print('[SHUTDOWN] Shutting down server...')
+    server.close()
 
 if __name__ == '__main__':
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
