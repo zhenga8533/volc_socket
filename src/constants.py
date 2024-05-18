1cc4d5508ed7b5e1851723c028c333ba8fa70f90
Allen Zheng <zhenga8533@gmail.com>
2024-05-18 16:03:46 -0400

    moved pings to constant

--- src/constants.py
+++ src/constants.py
@@ -3,8 +3,14 @@ import socket
 
 
 API_KEY = os.getenv('API_KEY')
-POWDER_WEBHOOK = os.getenv('POWDER_WEBHOOK')
+
+# Server Variables
 SERVER = socket.gethostbyname(socket.gethostname())
 PORT = int(os.getenv('PORT'))
 FORMAT = 'utf-8'
 BUFFER = 256
+
+# Discord Variables
+CH_PING = '<@&1240705901908852826>'
+DM_PING = '<@&1240705811580194878>'
+POWDER_WEBHOOK = os.getenv('POWDER_WEBHOOK')
